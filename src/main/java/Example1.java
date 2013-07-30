97e10362f94579ad57d4a576482a4627c69e4b92
Jamie Whitehouse <basil.whitehouse@gmail.com>
2013-07-30 15:09:35 -0400

    Expand example formatting
    Example formatting for:
    - class extends and implments
    - throwing exception

--- src/main/java/Example1.java
+++ src/main/java/Example1.java
@@ -1,8 +1,16 @@
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Properties;
+
 public class Example1
+  extends Properties
+  implements Serializable
 {
+  private static final long serialVersionUID = 4946535894651225728L;
+
   public int[] X = new int[]{1, 3, 5 7, 9, 11};
 
-  public void foo(boolean a, int x, int y, int z) {
+  public void foo(boolean a, int x, int y, int z) throws IOException {
     label1:
     do {
       try {

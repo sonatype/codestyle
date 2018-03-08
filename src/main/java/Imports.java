34eade2c0ee92b6495de7c9e2b06a4d231a913c5
Troy Neeriemer <tneeriemer@sonatype.com>
2018-03-08 08:32:31 -0600

    Relax star imports rule.  Add some java.lang examples in the Imports.java example.

--- src/main/java/Imports.java
+++ src/main/java/Imports.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.crypto.NullCipher;
 
 import static java.lang.Math.min;
@@ -5,7 +8,7 @@ import static java.lang.Math.min;
 public class Imports
 {
   public Imports() {
-
+    List list = new ArrayList();
     new NullCipher();
 
     try {

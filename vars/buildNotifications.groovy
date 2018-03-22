2a3e588005e35d4139f391088144efc961bd290c
Troy Neeriemer <tneeriemer@sonatype.com>
2018-03-22 14:19:04 -0500

    Update to support building via Jenkins.

---
+++ vars/buildNotifications.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+
+def call(currentBuild, env) {
+  def branch = gitBranch(env)
+  def defaultRecipients = 'builds-group@sonatype.com';
+
+  def buildStatus = currentBuild.currentResult
+
+  // Build status values found at http://javadoc.jenkins-ci.org/hudson/model/Result.html
+  switch (buildStatus) {
+    case ~/FAILURE|UNSTABLE/:
+      // Notify the developers who committed to the failed build, the initiator of the build and if on master
+      // then notify the build group.
+      sendEmailNotification(currentBuild, env,
+          [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+          'master' == branch ? defaultRecipients : '')
+
+      if ('master' == branch) {
+        hipchatSend room: 'iq-builds', message: "FAILURE: ${currentBuild.fullDisplayName}", color: 'RED', notify: true
+      }
+      break
+    case 'SUCCESS':
+      // previous failure on master
+      if ('master' == branch && currentBuild?.previousBuild?.result =~ /FAILURE|UNSTABLE/) {
+
+        // Notify the developers who committed to the failed build, the build initiator, and the build group.
+        sendEmailNotification(currentBuild, env,
+            [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+            defaultRecipients)
+        hipchatSend room: 'iq-builds', message: "SUCCESS: ${currentBuild.fullDisplayName}", color: 'GREEN', notify: true
+      }
+      else {
+        // only notify the person who initiated the build
+        sendEmailNotification(currentBuild, env)
+      }
+      break
+    case ~/ABORTED|NOT_BUILT/:
+      echo 'Not built.  No notification will be sent.'
+      break
+    default:
+      error 'Unable to determine build status.'
+      break
+  }
+}

b803a790fa2e0a131ee0144089fdbe693f46498b
Troy Neeriemer <tneeriemer@sonatype.com>
2021-07-08 12:56:53 -0500

    Update license text and Jenkins settings for OSS/public project. (#73)

--- checkstyle-checks/src/test/resources/ClassStructureEmptyLineCheckTest/bothEmptyLinesInterfaceSource.java
+++ checkstyle-checks/src/test/resources/ClassStructureEmptyLineCheckTest/bothEmptyLinesInterfaceSource.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
- * "Sonatype" is a trademark of Sonatype, Inc.
+ * Copyright (c) 2007-present Sonatype, Inc. All rights reserved.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-
 package com.sonatype;
 
 // has multiple trailing and preceeding empty lines
